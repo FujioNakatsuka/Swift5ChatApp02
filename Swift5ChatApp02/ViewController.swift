6554f2879c9fd766f40750e8c9125756bf788ca9
Fujio Nakatsuka <com.nifty.cbm01799>
2020-08-05 11:55:49 +0900

    Initial Commit

---
+++ Swift5ChatApp02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5ChatApp02
+//
+//  Created by 中塚富士雄 on 2020/08/05.
+//  Copyright © 2020 中塚富士雄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
